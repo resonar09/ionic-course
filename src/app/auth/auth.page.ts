2c4b7fe8d951c1d87c020edd619ec028a22042d8
resonar09 <darrenperez@gmail.com>
2019-06-17 09:30:10 -0400

    Login Switch ane New Offer

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -12,6 +12,7 @@ import { NgForm } from '@angular/forms';
 })
 export class AuthPage implements OnInit {
   isLoading = false;
+  isLogin = true;
   constructor(private authService: AuthService, private router: Router, private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
@@ -34,7 +35,22 @@ export class AuthPage implements OnInit {
   onLogout() {
     this.authService.onLogout();
   }
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
+  }
   onSubmit(form: NgForm) {
-    console.log(form);
+    // console.log(form);
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password);
+    if (this.isLogin) {
+      // Send a request to login servers
+    } else {
+      // Send a request to signup servers
+    }
+    this.onLogin();
   }
 }
